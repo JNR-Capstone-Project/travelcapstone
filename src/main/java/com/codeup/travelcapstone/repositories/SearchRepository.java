18033763d5958675ec559c79cf90c5b4b5ec26c1
robertolago <robertolagob@gmail.com>
2018-06-29 14:28:55 -0500

    Search repository fixed

--- src/main/java/com/codeup/travelcapstone/repositories/SearchRepository.java
+++ src/main/java/com/codeup/travelcapstone/repositories/SearchRepository.java
@@ -1,9 +1,10 @@
-//package com.codeup.travelcapstone.repositories;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface SearchRepository extends CrudRepository{
-//
-//
-//
-//}
+package com.codeup.travelcapstone.repositories;
+
+import com.codeup.travelcapstone.model.Search;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SearchRepository extends CrudRepository<Search,Long>{
+
+
+
+}
