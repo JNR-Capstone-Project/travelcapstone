5367e086e4273c4211bd15fe372bc7ead3fcefb8
Jake J <jakejohnson818@gmail.com>
2018-06-29 13:26:31 -0500

    Attempting to fix errors

---
+++ src/main/java/com/codeup/travelcapstone/repositories/Users.java
@@ -0,0 +1,8 @@
+package com.codeup.travelcapstone.repositories;
+
+import com.codeup.travelcapstone.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface Users extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
