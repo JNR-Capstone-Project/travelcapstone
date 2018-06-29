70b108ac7cb3e37a6e1d413b6f90018b8d9d3f6a
Jake J <jakejohnson818@gmail.com>
2018-06-29 10:46:48 -0500

    Initial commit to include .gitignore

---
+++ src/main/java/com/codeup/travelcapstone/TravelcapstoneApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.travelcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelcapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelcapstoneApplication.class, args);
+	}
+}
