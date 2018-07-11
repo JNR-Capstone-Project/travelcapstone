ffbc9bf3c639c49a06df707fd8d16cb342aca3b4
Jake J <jakejohnson818@gmail.com>
2018-07-11 11:51:25 -0500

    Set up reminder text notification system

--- src/main/java/com/codeup/travelcapstone/Jobs/ScheduledReminders.java
+++ src/main/java/com/codeup/travelcapstone/Jobs/ScheduledReminders.java
@@ -3,36 +3,51 @@ package com.codeup.travelcapstone.Jobs;
 
 import com.codeup.travelcapstone.model.Reminder;
 import com.codeup.travelcapstone.repositories.ReminderRepository;
+import com.codeup.travelcapstone.services.TwilioService;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 @Component
-public class ScheduledReminders {
+public class ScheduledReminders extends TwilioService{
     private ReminderRepository reminderRepo;
 
     public ScheduledReminders(ReminderRepository reminderRepo){
         this.reminderRepo = reminderRepo;
     }
 
-//    private static final Logger log = LoggerFactory.getLogger(ScheduledReminders.class);
+    private static final Logger log = LoggerFactory.getLogger(ScheduledReminders.class);
 
-    @Scheduled(fixedRate = 600000)
+    @Scheduled(fixedRate = 60000)
     public void fetchTime(){
         Iterable<Reminder> reminders = reminderRepo.findAll();
         if (reminders == null){
             System.out.println("no reminders");
         } else {
+            DateTimeFormatter dateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            LocalDateTime now = LocalDateTime.now();
+            String today = dateTime.format(now).substring(0, 10);
+            System.out.println(dateTime.format(now));
             for (Reminder reminder : reminders) {
                 long id = reminder.getId();
                 String action = reminder.getAction();
                 String reminderDate = reminder.getDate();
-                DateTime today = new DateTime();
                 System.out.println(reminderDate);
-                System.out.println(today);
+                if (reminderDate.substring(0, 10).equals(today)){
+                    System.out.println("We have a match!");
+                    String reminderTime = reminderDate.substring(12, 16);
+                    String message = action + " at: " + reminderTime;
+                    System.out.println(reminder.getAction() + reminder.getUser().getPhonenumber());
+                    sendText(message, reminder.getUser().getPhonenumber());
+                    reminderRepo.delete(id);
+                }
             }
+
             System.out.println("these are your reminders");
         }
     }
--- src/main/java/com/codeup/travelcapstone/TravelcapstoneApplication.java
+++ src/main/java/com/codeup/travelcapstone/TravelcapstoneApplication.java
@@ -2,8 +2,10 @@ package com.codeup.travelcapstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TravelcapstoneApplication {
 
 	public static void main(String[] args) {
