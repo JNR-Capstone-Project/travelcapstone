bbb6f2d57e059d286e70188e56e88f53ff1277fd
nestor salinas <nestorasm88@gmail.com>
2018-07-12 15:49:28 -0500

    chore: rendering map with markers for POI

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var locationAirport;
-var airportCode;
 const apiKey = document.getElementById("amadeus-API").value;
 
 /* =======================================================================
@@ -78,8 +76,15 @@ $(function() {
 ========================================================================*/
 
 
-// $(function () {
+$(function () {
 
+
+    if(document.getElementById("lowFlight") === null){
+
+        console.log("just checking");
+
+
+    } else {
 
         document.getElementById("lowFlight").addEventListener('click', function () {
 
@@ -90,38 +95,31 @@ $(function() {
             var return_date = "&return_date=" + $("#returnDate").val();
             var maximumPrice = "&max_price=" + $("#price").val();
             var currency = "&currency=USD";
-            airportCode=$("#destination").val();
             var resource_url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=' + apiKey + origin + destination + departure_date + return_date + maximumPrice + currency;
-            var airportLocationUrl='https://api.sandbox.amadeus.com/v1.2/location/'+$("#destination").val()+'?apikey='+apiKey;
 
-            var request=$.get(resource_url);
+            var request = $.get(resource_url);
             request.fail(function (current, status, error) {
                 console.log(status);
                 console.log(error);
             });
 
-            var airportLocation=$.get(airportLocationUrl);
-            airportLocation.done(function (response) {
-                   locationAirport=response.airports[0].location;
-                   console.log(locationAirport);
-            });
 
             request.done(function (response) {
                 console.log(response);
 
-               for (var i=0;i<response.results.length;i++){
-                   $('#flight').append(setFly(response.results[i]));
+                for (var i = 0; i < response.results.length; i++) {
+                    $('#flight').append(setFly(response.results[i]));
 
 
-               }
+                }
 
 
             });
 
             function setFly(fly) {
                 var htmlFly = "";
-                var trim = fly.itineraries[0].outbound.flights[0].departs_at.slice(11,16);
-                var xtrim = fly.itineraries[0].inbound.flights[0].departs_at.slice(11,16);
+                var trim = fly.itineraries[0].outbound.flights[0].departs_at.slice(11, 16);
+                var xtrim = fly.itineraries[0].inbound.flights[0].departs_at.slice(11, 16);
                 var airline = fly.itineraries[0].outbound.flights[0].marketing_airline;
 
 
@@ -136,20 +134,20 @@ $(function() {
                 var timeValue;
                 var timeV;
 
-                if (hours  > 0 && hours  <= 12) {
-                    timeValue= "" + hours;
+                if (hours > 0 && hours <= 12) {
+                    timeValue = "" + hours;
 
-                } else if (hours  > 12) {
-                    timeValue= "" + (hours - 12);
+                } else if (hours > 12) {
+                    timeValue = "" + (hours - 12);
 
                 } else if (hours == 0) {
-                    timeValue= "12";
+                    timeValue = "12";
 
                 }
 
                 if (xhours > 0 && xhours <= 12) {
                     timeV = "" + xhours;
-                } else if ( xhours > 12) {
+                } else if (xhours > 12) {
                     timeV = "" + (xhours - 12)
                 } else if (xhours == 0) {
                     timeV = "12";
@@ -171,19 +169,18 @@ $(function() {
                 htmlFly += "<td>" + timeV + "</td>";
                 htmlFly += "<td>" + fly.itineraries[0].inbound.duration + "</td>";
                 htmlFly += "<td>$" + fly.fare.total_price + "</td>";
-                if(airline === 'AA'){
+                if (airline === 'AA') {
 
                     htmlFly += "<td><a href='https://www.aa.com/homePage.do' ><img src='https://vignette.wikia.nocookie.net/logopedia/images/d/d7/American_Airlines_logo.svg/revision/latest/scale-to-width-down/340?cb=20130728031212' alt='none' class='logo2'/>" + "</a></td>";
                 }
-                if(airline === 'UA'){
-                    htmlFly += "<td><a href='https://www.united.com/ual/en/us/'><img src='https://vignette.wikia.nocookie.net/logopedia/images/0/0a/United_Airlines_2010.svg/revision/latest/scale-to-width-down/250?cb=20180505152143' alt='none' class='logo2'/>"  + "</a></td>";
+                if (airline === 'UA') {
+                    htmlFly += "<td><a href='https://www.united.com/ual/en/us/'><img src='https://vignette.wikia.nocookie.net/logopedia/images/0/0a/United_Airlines_2010.svg/revision/latest/scale-to-width-down/250?cb=20180505152143' alt='none' class='logo2'/>" + "</a></td>";
 
                 }
-                if (airline === 'DL'){
+                if (airline === 'DL') {
                     htmlFly += "<td><a href='https://www.delta.com/flight-search/book-a-flight'><img src='https://vignette.wikia.nocookie.net/logopedia/images/c/cc/DLCON.gif/revision/latest/thumbnail-down/width/98/height/20?cb=20101015204631' alt='none' class='logo2'/>" + "</a></td>";
 
                 }
-
 
 
                 htmlFly += "</tr>";
@@ -192,24 +189,10 @@ $(function() {
             }
 
 
-        // <th><i class="fa fa-building-o"></i>Departure</th>
-        //     <th><i class="icon_pin_alt"></i>Duration</th>
-        //     <th><i class="fa fa-money"></i> Return</th>
-        //     <th><i class="fa fa-money"></i> Duration</th>
-        //     <th><i class="icon_mobile"></i> Price</th>
-        //     <th><i class="icon_tool"></i> Book with Airline</th>
-
         });
 
-// });
-
-
-/* =======================================================================
-                    Hotel Api
-========================================================================*/
-
-
-
+    }
+});
 
 /* =======================================================================
                     Airport Hotel Map
@@ -217,79 +200,87 @@ $(function() {
 
 $(function () {
 
-    document.getElementById("cheapHotel").addEventListener('click', function () {
+    if(document.getElementById("cheapHotel") === null){
+        console.log("check 1 2");
+
+    } else {
+
+        document.getElementById("cheapHotel").addEventListener('click', function () {
 
 
-        var location = airportCode;
-        console.log(airportCode);
-        var checkIn = $("#checkIn").val();
-        console.log(checkIn);
-        var checkOut = $("#checkOut").val();
-        console.log(checkOut);
+            var location = airportCode;
+            console.log(airportCode);
+            var checkIn = $("#checkIn").val();
+            console.log(checkIn);
+            var checkOut = $("#checkOut").val();
+            console.log(checkOut);
 
 
-        function airportHotel(location, check_in, check_out) {
+            function airportHotel(location, check_in, check_out) {
 
-            $.get('https://api.sandbox.amadeus.com/v1.2/hotels/search-airport',
-                {
-                    dataType: 'json',
-                    apikey: apiKey,
-                    check_in: check_in,
-                    check_out: check_out,
-                    location: location
-                }).done(function (data) {
-                console.log(data.results);
-                // $(".container").html(" "); line to clean the results every time for a new search load
-                for (var i = 0; i < data.results.length; i++) {
-                    $("#hotels").append(setHotel(data.results[i]));
+                $.get('https://api.sandbox.amadeus.com/v1.2/hotels/search-airport',
+                    {
+                        dataType: 'json',
+                        apikey: apiKey,
+                        check_in: check_in,
+                        check_out: check_out,
+                        location: location
+                    }).done(function (data) {
+                    console.log(data.results);
+                    // $(".container").html(" "); line to clean the results every time for a new search load
+                    for (var i = 0; i < data.results.length; i++) {
+                        $("#hotels").append(setHotel(data.results[i]));
 
-                }
-            });
-        }
+                    }
+                });
+            }
 
 
-        airportHotel(location, checkIn, checkOut);
+            airportHotel(location, checkIn, checkOut);
 
 
 //row struture for the hotel
 
-        function setHotel(hotel) {
-            var htmlHotel = "";
-            var contact;
+            function setHotel(hotel) {
+                var htmlHotel = "";
+                var contact;
 
-            for (var i = 0; i < hotel.contacts.length; i ++){
-               var set = hotel.contacts[i];
+                for (var i = 0; i < hotel.contacts.length; i++) {
+                    var set = hotel.contacts[i];
+
+                }
+
+                if (set.type === 'URL') {
+                    contact = "<td>" + '<a href="url:' + hotel.contacts[2].detail + '">' + hotel.contacts[2].detail + "</a>" + "</td>";
+                }
+                else {
+                    contact = "<td>" + '<a href="tel:' + hotel.contacts[0].detail + '">' + hotel.contacts[0].detail + "</a>" + "</td>"
+                }
+
+
+                htmlHotel += "<tr>";
+                htmlHotel += "<td>" + hotel.property_name + "</td>";
+                htmlHotel += "<td>" + hotel.address.line1 + ', ' + hotel.address.city + ' ' + hotel.address.postal_code + "</td>";
+                htmlHotel += contact;
+                htmlHotel += "<td>" + "$" + hotel.total_price.amount + "</td>";
+                htmlHotel += "</tr>";
+                return htmlHotel;
 
             }
 
-            if(set.type === 'URL'){
-               contact = "<td>" + '<a href="url:' + hotel.contacts[2].detail + '">'+ hotel.contacts[2].detail +"</a>" + "</td>";
-            }
-            else{
-                contact = "<td>" + '<a href="tel:' + hotel.contacts[0].detail + '">' + hotel.contacts[0].detail + "</a>" + "</td>"
-            }
-
-
-            htmlHotel += "<tr>";
-            htmlHotel += "<td>" + hotel.property_name + "</td>";
-            htmlHotel += "<td>" + hotel.address.line1 + ', '+ hotel.address.city +' '+  hotel.address.postal_code   +"</td>";
-            htmlHotel += contact;
-            htmlHotel += "<td>" + "$" +hotel.total_price.amount + "</td>";
-            htmlHotel += "</tr>";
-            return htmlHotel;
-
-        }
-
-    });
-
+        });
+    }
 });
-
 
 /* =======================================================================
                     car rental
 ========================================================================*/
 
 $(function () {
+    if(document.getElementById("carRental") === null){
+        console.log("test for days");
+
+    } else {
     document.getElementById("carRental").addEventListener('click', function () {
 
         var loc = "&location=" + airportCode;
@@ -327,6 +318,7 @@ $(function () {
         }
 
     });
+    }
 });
 
 
@@ -338,51 +330,52 @@ $(function () {
 
 
 $(function () {
-
-    document.getElementById("ent").addEventListener('click', function () {
-
-
-
-//building the map
-
-        var mapOptions = {
-            zoom: 12,
-            center: {
-                lat: locationAirport.latitude,
-                lng: locationAirport.longitude
-            },
-            mapTypeId: google.maps.MapTypeId.TERRAIN,
-            draggable: true
-        };
-        var mapCanvas = document.getElementById('poiMap');
-        var map = new google.maps.Map(mapCanvas, mapOptions);
+    if(document.getElementById('poiPosts') === null ){
+        console.log('this is chill');
+    }else{
 
 
-//setting the main marker ...should be the user destination
+        document.getElementById('poiPosts').addEventListener('click', function () {
+
+            var locationAirport = 'BOS';
+            var airportLocationUrl = 'https://api.sandbox.amadeus.com/v1.2/location/' + locationAirport + '?apikey=' + apiKey;
+
+            var airportLocation = $.get(airportLocationUrl);
+            airportLocation.done(function (response) {
+                locationAirport = response.airports[0].location;
+
+                //building the map
+                var mapOptions = {
+                    zoom: 12,
+                    center: {
+                        lat: locationAirport.latitude,
+                        lng: locationAirport.longitude
+                    },
+                    mapTypeId: google.maps.MapTypeId.TERRAIN,
+                    draggable: true
+                };
+                var mapCanvas = document.getElementById('poiMap');
+                var map = new google.maps.Map(mapCanvas, mapOptions);
+                // obtain the Poi JSON  passing a location and setting the markers for the destination
 
 
-        function setMarker(location) {
-            marker.setPosition(location);
-        }
+                $.get('https://api.sandbox.amadeus.com/v1.2/points-of-interest/yapq-search-circle?',
+                    {
+                        dataType: 'json',
 
-        new google.maps.Marker({
-            position: {
-                lat: locationAirport.latitude,
-                lng: locationAirport.longitude
-            },
-            map: map
-        });
-
-
-        function setMarker(location) {
-            marker.setPosition(location);
-
-        }
+                        apikey: apiKey,
+                        latitude: locationAirport.latitude,
+                        longitude: locationAirport.longitude,
+                        radius: '40'
+                    }).done(function (data) {
+                    console.log(data);
 
 
-// function to get a marker for each poi on the JSON
-        function poiToMarker(data) {
-            for (var i = 0; i < data.points_of_interest.length; i++) {
+
+                    poiToMarker(data);
+
+
+                });
                 new google.maps.Marker({
                     position: {
                         lat: locationAirport.latitude,
@@ -390,80 +383,67 @@ $(function () {
                     },
                     map: map
                 });
-                $("#poiPosts").append(createReport(data.points_of_interest[i]));
-
-            }
 
 
-        }
+
+//setting the main marker ...should be the user destination
 
 
-// obtain the Poi JSON  passing a location and setting the markers for the destination
+                function setMarker(location) {
+                    marker.setPosition(location);
+                }
 
 
-        $.get('https://api.sandbox.amadeus.com/v1.2/points-of-interest/yapq-search-circle?',
-            {
-                dataType: 'json',
+                // function to get a marker for each poi on the JSON
+                function poiToMarker(data) {
+                    for (var i = 0; i < data.points_of_interest.length; i++) {
 
-                apikey: apiKey,
-                latitude: locationAirport.latitude,
-                longitude: locationAirport.longitude,
-                radius: '40'
-            }).done(function (data) {
-            console.log(data);
-            poiToMarker(data);
+                        console.log(data[0]);
 
-        });
+                        // new google.maps.Marker({
+                        //     position: {
+                        //         lat: location[0].latitude,
+                        //         lng: location[1].longitude
+                        //     },
+                        //     map: map
+                        // });
+
+
+                        $("#poiPosts").append(createReport(data.points_of_interest[i]));
+
+                    }
+
+
+                }
+
+
+
+
+
+
+                console.log("que onda, vas a sevir on no?");
+
 
 
 //creating the div for the view
 
-        function createReport(poi) {
-            var htmlPlace = "";
-            htmlPlace += "<div class='screen'>";
-            htmlPlace += "<h3>" + poi.title + "</h3>" + "\n";
-            htmlPlace += "<img src=" + poi.main_image + ">" + "\n";
-            htmlPlace += "<p> " + poi.details.description + "</p>" + "\n";
-            htmlPlace += "</div>";
-            return htmlPlace
-        }
+                function createReport(poi) {
+                    var htmlPlace = "";
+                    htmlPlace += "<div class='screen'>";
+                    htmlPlace += "<h3>" + poi.title + "</h3>" + "\n";
+                    htmlPlace += "<img src=" + poi.main_image + ">" + "\n";
+                    htmlPlace += "<p> " + poi.details.description + "</p>" + "\n";
+                    htmlPlace += "</div>";
+                    return htmlPlace
+                }
+
+
+            });
+
+
+
 
 
     });
-});
-
-/* =======================================================================
-                    Airport Hotel Map
-========================================================================*/
-
-// function airportHotel(location, check_in,check_out) {
-//
-//
-//     $.get('https://api.sandbox.amadeus.com/v1.2/hotels/search-airport',
-//         {
-//             dataType: 'json',
-//             apikey: apiKey,
-//             check_in: check_in,
-//             check_out: check_out,
-//             location: location
-//         }).done(function (data) {
-//         console.log(data.results);
-//         // $(".container").html(" "); line to clean the results every time for a new search load
-//         for (var i =0;i< data.results.length; i++)
-//         {
-//             $("#hotels").append(setHotel(data.results[i]));
-//
-//         }
-//     })
-// }
-document.getElementById("#editSavedSearch").addEventListener('click', function () {
-// $("#editSavedSearch").click(function() {
-    // var id = $(this).find("td:last-child").text();
-    console.log("hello from the bottom");
-});
-
-
-$(function () {
-
-
+    }
 });
